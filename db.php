b7e29336255f1b2ba13fbb2905cde004a2429641
hussein mhommed <123593929+Hessan-harb@users.noreply.github.com>
2024-06-30 09:11:22 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,18 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'scandiweb_test2';
-    private $username = 'root';
-    private $password = '';
+    private $host = 'bn12utiqveliehfcciia-mysql.services.clever-cloud.com';
+    private $db_name = 'bn12utiqveliehfcciia';
+    private $username = 'u2jcflna7lmkhbex';
+    private $password = '1dwLsfeDvWMH5YvCzWT5';
+    private $port = 3306;
     public $pdo;
 
     public function getConnection() {
         $this->pdo = null;
 
         try {
-            $this->pdo = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->pdo->exec("set names utf8");
+            $this->pdo = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name . ";charset=utf8mb4", $this->username, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
@@ -19,5 +20,6 @@ class Database {
         return $this->pdo;
     }
 }
+
 
 ?>
